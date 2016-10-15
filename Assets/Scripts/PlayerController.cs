e4927ec6163c6a31dd60ba867970439ade60cef7
chakabaka1027 <alexanderdcha@gmail.com>
2016-10-15 16:19:43 -0400

    changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -38,7 +38,6 @@ public class PlayerController : MonoBehaviour {
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
 			if (hit){
-//				hit.collider.gameObject.GetComponent<SpriteRenderer>().material.color = Color.red;
 				peach.transform.position = hit.point;
 
 			}
