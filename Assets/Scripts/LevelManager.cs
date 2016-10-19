403c63172de6f671c3a09826851c479e12f82089
chakabaka1027 <alexanderdcha@gmail.com>
2016-10-19 03:13:11 -0400

    cleaned up level manager script to not include old code from original version

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -10,7 +10,6 @@ public class LevelManager : MonoBehaviour {
 
 	char[] delimiterChars = { ' ', ',', '(', ')'};
 
-
 	public float TileSize{
 		get { return tilePrefabs[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x * 3;}
 	}
@@ -21,8 +20,6 @@ public class LevelManager : MonoBehaviour {
 
 	void CreateLevel(){
 		string[] mapData = ReadLevelText();
-		//new text beginning
-
 		worldStart = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height));
 
 		//parsing each delimited element
@@ -36,28 +33,9 @@ public class LevelManager : MonoBehaviour {
 
 			PlaceTile(tileType, mapX, mapY, worldStart);
 		}
-
-
-
-
-		//new text end
-//		int mapX = mapData[0].ToCharArray().Length;
-//		int mapY = mapData.Length;
-
-//		worldStart = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height));
-//
-//		for (int y = 0; y < mapY; y++){
-//			char[] newTiles = mapData[y].ToCharArray();
-//
-//			for (int x = 0; x < mapX; x++){
-//				PlaceTile(newTiles[x].ToString(), x, y, worldStart);
-//			}
-//		}
 	}
 
 	private void PlaceTile(int tileType, int x, int y, Vector3 worldStart){
-//		int tileIndex = int.Parse(tileType);
-
 		GameObject newTile = Instantiate(tilePrefabs[tileType]);
 		newTile.transform.position = new Vector3(worldStart.x + TileSize * x, worldStart.y - TileSize * y, 0);
 		newTile.transform.parent = gameObject.transform;
